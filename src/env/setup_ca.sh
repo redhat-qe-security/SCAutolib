5e17f8c7012864bbb75a781b6c833b1b716ea6a9
Pavel Yadlouski <pyadlous@redhat.com>
2021-03-29 11:47:17 +0200

    Change deploying of ca and virtual card

--- src/env/setup_ca.sh
+++ src/env/setup_ca.sh
@@ -28,13 +28,13 @@ fi
 SOPIN='12345678'
 PIN='123456'
 export GNUTLS_PIN=$PIN
-VIRT=$DIR/virt_card
-NSSDB=$DIR/virt_card/db
+VIRT=$DIR
+NSSDB=$VIRT/db
 CONF=$VIRT/conf
 NAME=localuser1
 
-mkdir $VIRT
-cp -r $DIR/SCAutolib/src/env/conf $CONF
+#mkdir $VIRT
+#cp -r $DIR/SCAutolib/src/env/conf $CONF
 
 if [[ ! -f "$CONF/softhsm2.conf" ]]
 then 
@@ -109,32 +109,5 @@ cp /usr/lib/systemd/system/pcscd.service /etc/systemd/system/
 sed -i 's/ --auto-exit//' /etc/systemd/system/pcscd.service
 systemctl daemon-reload
 systemctl restart pcscd
-
-######################################
-# Install virt_cacard service
-######################################
-dnf -y install virt_cacard
-cp $CONF/virt_cacard.service /etc/systemd/system/virt_cacard.service
-sed -i "s,<TESTDIR>,$VIRT,g" /etc/systemd/system/virt_cacard.service
-systemctl daemon-reload
-echo 'disable-in: virt_cacard' >> /usr/share/p11-kit/modules/opensc.module
-systemctl restart pcscd virt_cacard
-sleep 10
-
-mkdir /home/localuser1/.ssh
-ssh-keygen -D /usr/lib64/pkcs11/opensc-pkcs11.so > ~localuser1/.ssh/authorized_keys
-chown -R localuser1:localuser1 ~localuser1/.ssh/
-chmod 700 ~localuser1/.ssh/
-chmod 600 ~localuser1/.ssh/authorized_keys
-
-cp $CONF/sssd.conf /etc/sssd/sssd.conf
-chmod 600 /etc/sssd/sssd.conf
-cat $VIRT/rootCA.crt > /etc/sssd/pki/sssd_auth_ca_db.pem
-
-systemctl stop pcscd.service pcscd.socket virt_cacard sssd
-rm -rf /var/lib/sss/{db,mc}/*
-systemctl start pcscd sssd
-
-pip3 install avocado-framework pexpect
 
 exit 0
