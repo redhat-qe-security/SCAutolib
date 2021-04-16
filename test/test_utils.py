a70f18d445143c53f1af8a7f3a34436d4c52c028
x00Pavel <pavel.yadlouski@gmail.com>
2021-04-16 18:22:15 +0200

    add test for CI

--- test/test_utils.py
+++ test/test_utils.py
@@ -7,24 +7,24 @@ CUR_PATH = path.dirname(path.abspath(__file__))
 FILES = f"{CUR_PATH}/files"
 
 
-@pytest.mark.local
+@pytest.mark.ci
 def test_service_restart():
     """Test for restarting the service"""
-    rc = utils.restart_service("cron")
+    rc = utils.restart_service("sssd")
     assert rc == 0
     stat = system("systemctl status cron")
     assert stat == 0
 
-@pytest.mark.local
+@pytest.mark.ci
 def test_service_restart_fail():
     """Test for fault of service restart."""
     copy(f"{FILES}/test.service", "/etc/systemd/system/test.service")
-    rc = system("systemctl daemon-reload")
+    rc = system("sudo systemctl daemon-reload")
     assert rc == 0
     rc = utils.restart_service("test")
     assert rc != 0
     remove("/etc/systemd/system/test.service")
-    rc = system("systemctl daemon-reload")
+    rc = system("sudo systemctl daemon-reload")
     assert rc == 0
 
 
