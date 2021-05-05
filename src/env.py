90b964b0bf9e1005ddc43c99964d525cc466186f
Pavel Yadlouski <pyadlous@redhat.com>
2021-05-05 18:08:17 +0200

    Fix read_config function for reading nested dicts

--- src/env.py
+++ src/env.py
@@ -38,7 +38,16 @@ def _prep_tmp_dirs():
         mkdir(BACKUP)
 
 
-def _create_sssd_conf(username="localuser1"):
+def _create_sssd_conf(local_user=None, krb_user=None):
+    """
+    Update the content of the sssd.conf file. If file exists, it would be store
+    to the backup folder and content in would be edited for testing purposes.
+    If file doesn't exist, it would be created and filled with default options.
+    Args:
+        local_user: username for local user with smart card to add the match rule.
+        krb_user: username for kerberos user with smart card to add the match rule.
+    """
+
     holder = "#<{holder}>\n"
     content = []
     if exists("/etc/sssd/sssd.conf"):
@@ -49,7 +58,15 @@ def _create_sssd_conf(username="localuser1"):
         for index, line in enumerate(content):
             if match(r"^\[(.*)]\n$", line):
                 content[index] = line + holder.format(holder=line.rstrip("\n"))
-    #     TODO: add mathrule for testing user
+        if local_user:
+            rule = f"\n[certmap/shadowutils/{local_user}]\n" \
+                   f"matchrule = <SUBJECT>.*CN={local_user}.*\n" \
+                   f"#<[certmap/shadowutils/{local_user}]>\n"
+            content.append(rule)
+        if krb_user:
+            pass
+            # FIXME: add rule for kerberos user
+            # content.append(rule)
     else:
         content = ["[sssd]\n",
                    "debug_level = 9\n",
@@ -67,11 +84,14 @@ def _create_sssd_conf(username="localuser1"):
                    "\n[domain/shadowutils]\n",
                    "debug_level = 9\n",
                    "id_provider = files\n",
-                   "#<[domain/shadowutils]>\n",
-
-                   f"\n[certmap/shadowutils/{username}]\n",
-                   f"matchrule = < SUBJECT >.*CN = {username}.*\n",
-                   f"#<[certmap/shadowutils/{username}]>\n"]
+                   "#<[domain/shadowutils]>\n"]
+        if local_user:
+            content.append(f"\n[certmap/shadowutils/{local_user}]\n"
+                           f"matchrule = < SUBJECT >.*CN = {local_user}.*\n"
+                           f"#<[certmap/shadowutils/{local_user}]>\n")
+        if krb_user:
+            pass
+        # FIXME: add rule for kerberos user
 
     with open("/etc/sssd/sssd.conf", "w") as f:
         f.write("".join(content))
@@ -81,12 +101,48 @@ def _create_softhsm2_conf():
     pass
 
 
-@click.command()
-@click.option("--username", "-u", type=click.STRING, help="Username to be set in config files (e.g sssd.conf)")
-def prepair(username):
-    _prep_tmp_dirs()
+def _read_config(config, items: [str] = None) -> dict or list:
+    """
+    Read data from the configuration file and return require items or full
+    content.
 
-    _create_sssd_conf(username)
+    Args:
+        config: path to configuration file
+        items: list of items to extracrt from the configuration file.
+               If None, full contant would be returned
+
+    Returns: dictionary with full contant or list with required items
+    """
+    with open(config, "r") as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+        assert data, "Data are not loaded correctly."
+
+    if items is None:
+        return data
+    return_list = []
+    for item in items:
+        parts = item.split(".")
+        value = data
+        for part in parts:
+            try:
+                if value is None:
+                    raise KeyError
+                value = value.get(part)
+
+                if part == parts[-1]:
+                    return_list.append(value)
+            except KeyError:
+                log.debug(f"Key {part} not present in the configuration file. Skip.")
+                break
+    return return_list
+
+
+@click.command()
+@click.option("--conf", "-c", type=click.Path(), help="Path to YAML file with configurations")
+def prepair(conf):
+    _prep_tmp_dirs()
+    usernames = _read_config(conf, items=["variables.local_user.name", "variables.krb_user.name"])
+    _create_sssd_conf(*usernames)
     # TODO: create softhsm2.conf file
     _create_softhsm2_conf()
     # TODO: create .cnf files for certificates
@@ -113,11 +169,10 @@ def setup_ca(work_dir, conf):
 
     log.debug("Start setup of local CA")
 
-    with open(conf, "r") as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
+    src, user = _read_config(conf, items=["configs.dir", "variables"])
     conf_dir = f"{work_dir}/conf"
-    copytree(realpath(data["configs"]["dir"]), conf_dir)
-    user = data["variables"]["user"]
+    copytree(realpath(src), conf_dir)
+    # user = data["variables"]["user"]
     print(work_dir)
     out = subp.run(["bash", SETUP_CA, "--dir", work_dir,
                     "--username", user["name"],
