d1ad40402df6a1343f1c52e48f5d85a6e4726409
Pavel Yadlouski <46033323+x00Pavel@users.noreply.github.com>
2021-07-15 09:13:01 +0200

    Sync with master and parametrization branches (#4)
    * Sync with master and paramterization
    
    * Refactoring
    
    * Fix for RHEL 9
    
    * Update docs
    
    * Delete GitHub action
    
    * Delete ansible playbooks
    
    Co-authored-by: Pavel Yadlouski <pyadlous@redhat.com>

--- __init__.py
+++ __init__.py
@@ -1,2 +1,21 @@
 import logging
+from sys import stdout
+
+
+handler = logging.StreamHandler(stdout)
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt="%H:%M:%S")
+handler.setFormatter(formatter)
+
+# Basic logger
 log = logging.getLogger("base")
+log.setLevel(logging.DEBUG)
+
+log.addHandler(handler)
+
+# Logger for environment events
+env_logger = logging.getLogger("env")
+env_logger.setLevel(logging.DEBUG)
+
+env_logger.addHandler(handler)
