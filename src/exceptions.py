3c96334cf92446383d420693eac4e64de3d047c5
x00Pavel <pavel.yadlouski@gmail.com>
2021-08-05 18:21:08 +0200

    Add unit tests for env.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -16,3 +16,12 @@ class PatternNotFound(SCAutolibException):
         self.msg = msg
         self.pattern = pattern
         super().__init__(self.msg)
+
+
+class NoDirProvided(SCAutolibException):
+    def __init__(self, parameter: str = None, msg: str = "No directory is provided"):
+        self.msg = msg
+        self.parameter = parameter
+        if self.parameter is not None:
+            msg += f" for parameter {self.parameter}"
+        super().__init__(self.msg)
