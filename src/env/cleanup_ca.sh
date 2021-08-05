0e8bb95d97068b513d956f05b639a3a13853dca2
Pavel Yadlouski <46033323+x00Pavel@users.noreply.github.com>
2021-08-05 13:09:42 +0200

    Package installation (#15)
    * Fix sssl error on adding IPA user, add smart card creating on adding IPA user
    
    * Add custom exceptions for the library
    
    * Add check for zero return code of the cmd in run_cmd command
    
    * Add log funtions to separated file
    
    * Remove redundatn requirements
    
    * Add installation of vpcd directly to the script

--- src/env/cleanup_ca.sh
+++ src/env/cleanup_ca.sh
@@ -2,27 +2,10 @@
 
 set -e
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
+. "$(dirname $0)/logs.sh" || exit 1
 
 ENV_PATH="$(pwd)/../.env"
 
-function log() {
-  echo -e "${GREEN}${bold}[LOG $(date +"%T")]${normal}${NC} $1"
-}
-
-function err() {
-  echo -e "${RED}${bold}[ERROR $(date +"%T")]${normal}${NC} $1"
-  exit 1
-}
-
-function warn() {
-  echo -e "${YELLOW}${bold}[WARNING $(date +"%T")]${normal}${NC} $1"
-}
 
 while (("$#")); do
   case "$1" in
--- src/env/setup_vm.sh
+++ src/env/setup_vm.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/bash
 # author: Pavel Yadlouski <pyadlous@redhat.com>
-#!/usr/bin/bash
-set -e
-#trap read debug
+set -xe
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-GREEN='\033[0;32m'
+. "$(dirname $0)/logs.sh" || exit 1
+
 rx='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
 
 NAME='default-name'
@@ -19,22 +14,6 @@ NFS_DIR=1
 KEY=""
 RUN=0
 REPO_FILE="$(dirname $(realpath "$0"))/redhat.repo"
-
-function help() {
-  echo "Script for deploying local VM"
-  echo -e "\t-h | --help -- this massage"
-  echo -e "\t-n | -name -- name of VM. This name you will see in virt-manager"
-  echo -e "\t-s -- bash script that would be uploaded on the VM and the executed"
-  echo -e "\t-k | --key -- ssh public key that would be copied to VM"
-  echo -e "\t-r -- run given script on target host or now (default script is not executed)"
-  echo -e "\t-R | --repo-file -- file with repos for given image (default in $REPO_FILE)"
-
-  exit 0
-}
-
-function log() {
-  echo -e "${GREEN}${bold}[LOG $(date +"%T")]${normal}${NC} $1"
-}
 
 function err() {
   echo -e "${RED}${bold}[ERROR $(date +"%T")]${normal}${NC} $1"
@@ -146,14 +125,31 @@ do
 done
 echo
 
+
 ip_rhel8=$(virsh domifaddr "$NAME" | grep -o -E "$rx\.$rx\.$rx\.$rx")
 log "IP address of the VM: ${bold}${ip_rhel8}${normal}"
+
+echo -e \
+"Host $NAME
+    Hostname $ip_rhel8
+  	Preferredauthentications publickey
+  	User root
+  	UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no
+    IdentityFile $KEY\n" >> "$(dirname "$KEY")"/config
+log "New entry is created in the $(dirname "$KEY")/config for address $ip_rhel8 with name $NAME"
 
 ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "dnf update -y"
 log "Updating system complete"
 
-ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "dnf install vim gdm -y "
+ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "dnf install vim -y "
 log "VIM installed"
+
+ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "dnf install gdm -y "
+log "GDM installed"
+
+ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "dnf install python3 python3-pip -y "
+log "Python3 installed"
 
 ssh -o StrictHostKeyChecking=no -i "$KEY" root@"$ip_rhel8" "yum groupinstall 'Smart Card Support' -y"
 log "Group Smart Card Support is installed"
@@ -210,16 +206,6 @@ then
     log "Script ${SCRIPT} for ${ip_rhel8} is finished"
   fi
 fi
-
-echo -e \
-"Host $NAME
-    Hostname $ip_rhel8
-  	Preferredauthentications publickey
-  	User root
-  	UserKnownHostsFile /dev/null
-    StrictHostKeyChecking no
-    IdentityFile $KEY\n" >> "$(dirname "$KEY")"/config
-log "New entry is created in the $(dirname "$KEY")/config for address $ip_rhel8 with name $NAME"
 
 virsh snapshot-create-as --domain "$NAME" --name "initial"
 
