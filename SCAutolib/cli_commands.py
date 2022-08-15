339299aaa6fbf73affdaf54d83e2cb7e9739da86
x00Pavel <pavel.yadlouski@gmail.com>
2022-08-15 14:53:46 +0200

    Add more specifications for command options

--- SCAutolib/cli_commands.py
+++ SCAutolib/cli_commands.py
@@ -4,24 +4,46 @@ Implementation of CLI commands for SCAutolib.
 
 import click
 from SCAutolib.controller import Controller
+import coloredlogs
+from enum import Enum, auto
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--force', "-f", is_flag=True, default=False, show_default=True,
+              help="Force the command to overwrite configuration if it exists.")
+@click.option("--verbose", "-v", default="INFO", show_default=True,
+              type=click.Choice(
+                  ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+              help="Increase verbosity. Max level is 3.")
+@click.pass_context
+def cli(ctx, force, verbose):
+    coloredlogs.set_level(verbose)
+    ctx.ensure_object(dict)
+    ctx.obj["FORCE"] = force
 
 
 @click.command()
 @click.option("--ca-type", "-t", required=False, default='all',
-              show_default=True,
+              show_default=True, type=click.Choice(['all', 'local', 'ipa'], case_sensitive=False),
               help="Type of the CA to be configured. If not set, all CA's "
                    "from the config file would be configured")
 @click.option("--conf-file", "-c", required=False, default="./conf.json",
               type=click.Path(exists=True, resolve_path=True),
-              show_default=True)
-def setup_ca(conf_file, ca_type):
-    cnt = Controller(conf_file, {"ip_addr": "10.10.10.10"})
-    cnt.setup_ipa_client()
+              show_default=True, metavar="<conf-file>")
+@click.pass_context
+def setup_ca(ctx, conf_file, ca_type):
+    """
+    Configure the CA's in the config file. If more than one CA is
+    specified, specified CA type would be configured.
+    """
+    cnt = Controller(conf_file)
+    if ca_type == 'all':
+        cnt.setup_local_ca(force=ctx.obj["FORCE"])
+        cnt.setup_ipa_client(force=ctx.obj["FORCE"])
+    elif ca_type == 'local':
+        cnt.setup_local_ca(force=ctx.obj["FORCE"])
+    elif ca_type == 'ipa':
+        cnt.setup_ipa_client(force=ctx.obj["FORCE"])
 
 
 @click.command()
@@ -31,6 +53,7 @@ def setup_ca(conf_file, ca_type):
 @click.option("--install-missing", "-i", required=False, default=False,
               is_flag=True)
 def prepare(conf, force, gdm, install_missing):
+    """Configure entire system for smart cards based on the config file."""
     cnt = Controller(conf)
     cnt.prepare(force, gdm, install_missing)
 
