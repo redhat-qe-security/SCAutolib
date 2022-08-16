8f0572f8b2cfc2d5b26a26bb6a8296802687a22b
x00Pavel <pavel.yadlouski@gmail.com>
2022-08-16 15:37:47 +0200

    Move conf parameter to command group and add controller to command context

--- SCAutolib/cli_commands.py
+++ SCAutolib/cli_commands.py
@@ -9,17 +9,24 @@ from SCAutolib import logger
 
 
 @click.group()
+@click.option("--conf", "-c",
+              required=True,
+              default="./conf.json",
+              type=click.Path(exists=True, resolve_path=True),
+              show_default=True,
+              help="Path to JSON configuration file.")
 @click.option('--force', "-f", is_flag=True, default=False, show_default=True,
               help="Force the command to overwrite configuration if it exists.")
 @click.option("--verbose", "-v", default="INFO", show_default=True,
               type=click.Choice(
                   ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
-              help="Increase verbosity. Max level is 3.")
+              help="Verbosity level.")
 @click.pass_context
-def cli(ctx, force, verbose):
+def cli(ctx, force, verbose, conf):
     logger.setLevel(verbose)
     ctx.ensure_object(dict)  # Create a dict to store the context
     ctx.obj["FORCE"] = force  # Store the force option in the context
+    ctx.obj["CONTROLLER"] = Controller(conf)
 
 
 @click.command()
@@ -30,18 +37,13 @@ def cli(ctx, force, verbose):
               show_default=True,
               help="Type of the CA to be configured. If not set, all CA's "
                    "from the config file would be configured")
-@click.option("--conf-file", "-c",
-              required=False,
-              default="./conf.json",
-              type=click.Path(exists=True, resolve_path=True),
-              show_default=True)
-@click.pass_context  # to pass context (ctx) parameter to the function
-def setup_ca(ctx, conf_file, ca_type):
+@click.pass_context
+def setup_ca(ctx, ca_type):
     """
     Configure the CA's in the config file. If more than one CA is
     specified, specified CA type would be configured.
     """
-    cnt = Controller(conf_file)
+    cnt = ctx.obj["CONTROLLER"]
     if ca_type == 'all':
         cnt.setup_local_ca(force=ctx.obj["FORCE"])
         cnt.setup_ipa_client(force=ctx.obj["FORCE"])
@@ -52,27 +54,25 @@ def setup_ca(ctx, conf_file, ca_type):
 
 
 @click.command()
-@click.option("--conf", "-c", required=True)
-@click.option("--force", "-f", required=False, default=False, is_flag=True)
 @click.option("--gdm", "-g", required=False, default=False, is_flag=True)
 @click.option("--install-missing", "-i", required=False, default=False,
               is_flag=True)
-def prepare(conf, force, gdm, install_missing):
+@click.pass_context
+def prepare(ctx, gdm, install_missing):
     """Configure entire system for smart cards based on the config file."""
-    cnt = Controller(conf)
-    cnt.prepare(force, gdm, install_missing)
+    ctx.obj["CONTROLLER"].prepare(ctx.obj["FORCE"], gdm, install_missing)
 
 
 @click.command()
-@click.option("--conf", "-c", required=True)
-@click.option("--force", "-f", required=False, default=False, is_flag=True)
 @click.option("--name", "-n", required=True, default=None)
-def setup_user(name, conf, force):
-    cnt = Controller(conf)
+@click.pass_context
+def setup_user(ctx, name):
+    """Configure user with smart cards (if set) based on the config file."""
+    cnt = ctx.obj["CONTROLLER"]
     user_dict = cnt.get_user_dict(name)
     cnt.init_ca(user_dict["local"])
-    user = cnt.setup_user(user_dict, force)
-    cnt.enroll_card(user, force)
+    user = cnt.setup_user(user_dict, ctx.obj["FORCE"])
+    cnt.enroll_card(user, ctx.obj["FORCE"])
 
 
 cli.add_command(setup_ca)
