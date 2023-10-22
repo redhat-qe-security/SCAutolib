3e93111d322ac4da9461ef672ae7636195370b00
Marek Havrila <mhavrila@gmail.com>
2023-10-22 23:21:00 +0200

    Separate user and card objects
    Card object separates from user object. Many
    attributes are moved to card and User class is
    simplyfied to the extent that it can be unified
    with BaseUser class. Methods that were in User
    class are mostly moved to Card clases.

--- SCAutolib/models/user.py
+++ SCAutolib/models/user.py
@@ -3,13 +3,11 @@ This module defines the User and IPAUser classes which can be used
 to represent system and IPA users.
 
 The classes contain the usual properties that defines a user, like username,
-password, smart card pin, etc.
+password, etc.
 
 The classes implement add_user and delete_user methods which can be used to
 create or remove a specified user in the system or in the specified IPA server.
 """
-from shutil import rmtree
-
 import json
 import pwd
 import python_freeipa
@@ -17,37 +15,28 @@ from pathlib import Path, PosixPath
 
 from SCAutolib import run, logger, LIB_DUMP_USERS
 from SCAutolib.exceptions import SCAutolibException
-from SCAutolib.models import card as card_model
 from SCAutolib.models.CA import IPAServerCA
-from SCAutolib.models.file import OpensslCnf
 
 
-class BaseUser:
+class User:
+    """
+    User represents general system user.
+    """
     username: str = None
     password: str = None
-    pin: str = None
     dump_file: Path = None
-    _cnf: OpensslCnf = None
-    _key: Path = None
-    _cert: Path = None
-    card_dir: Path = None
-    _card: card_model.Card = None
-    local: bool = None
+    user_type: str = None
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.user_type = "local"
         self.dump_file = LIB_DUMP_USERS.joinpath(f"{self.username}.json")
 
     def to_dict(self):
         # Retype patlib.Path object to str
         d = {k: str(v) if type(v) in (PosixPath, Path) else v
              for k, v in self.__dict__.items()}
-
-        if self._card and isinstance(self._card, card_model.VirtualCard):
-            d.pop("_card")
-            d["card"] = str(self._card.dump_file)
-
         return d
 
     @staticmethod
@@ -59,45 +48,34 @@ class BaseUser:
         :type json_file: pathlib.Path
         :param kwargs: dictionary of additional values needed to initialise the
             object
-        :return: object of local or IPA user
+        :type kwargs: dict
+        :return: user object
         :rtype: SCAutolib.models.user.User or SCAutolib.models.user.IPAUser
         """
         with json_file.open("r") as f:
             cnt = json.load(f)
 
-        if "card_dir" not in cnt:
-            user = BaseUser(username=cnt["username"], password=cnt["password"])
-        elif cnt["local"]:
-            user = User(local=cnt["local"],
-                        username=cnt["username"],
-                        card_dir=Path(cnt["card_dir"]),
-                        password=cnt["password"],
-                        pin=cnt["pin"],
-                        key=cnt["_key"],
-                        cert=cnt["_cert"])
+        if cnt["user_type"] == "local":
+            user = User(username=cnt["username"],
+                        password=cnt["password"])
+
         else:
             if "ipa_server" not in kwargs:
                 raise SCAutolibException("IPA Server object does not provided. "
                                          "Can't load IPA user.")
 
             user = IPAUser(ipa_server=kwargs["ipa_server"],
-                           local=cnt["local"],
                            username=cnt["username"],
-                           card_dir=Path(cnt["card_dir"]),
-                           password=cnt["password"],
-                           pin=cnt["pin"],
-                           key=cnt["_key"],
-                           cert=cnt["_cert"])
+                           password=cnt["password"])
+
         logger.debug(f"User {user.__class__} is loaded: {user.__dict__}")
-        if "card" in cnt:
-            return user, Path(cnt["card"])
+
         return user
 
     def add_user(self):
         """
         Add user to the local system with `useradd` bash command and set
         password for created user.
-        :return:
         """
         try:
             pwd.getpwnam(self.username)
@@ -112,148 +90,24 @@ class BaseUser:
             run(cmd, check=True)
             cmd = ["passwd", self.username, "--stdin"]
             run(cmd, input=self.password)
-            logger.info(f"User {self.username} is present on the system")
-
-
-class User(BaseUser):
-    """
-    Generic class to represent system users.
-    """
-
-    def __init__(self, username: str, password: str, pin: str,
-                 cnf: Path = None, key: Path = None, cert: Path = None,
-                 card_dir: Path = None, local: bool = True):
-
-        """
-        :param username: Username for the system user
-        :type username: str
-        :param password: Password for the system user
-        :type password: str
-        :param pin: Smart card pin for the system user
-        :type pin: str
-        :param cnf: CNF file to be associated with the user
-        :type cnf: Path
-        :param key: Key to be associated with the user
-        :type key: Path
-        :param cert: Certificate to be associated with the user.
-        :type cert: Path
-        :param card_dir: Directory for the card. If None, standard
-            home directory would be used (/home/<username>)
-        :type card_dir: Path
-        """
-
-        self.username = username
-        self.password = password
-        self.pin = pin
-        self.dump_file = LIB_DUMP_USERS.joinpath(f"{self.username}.json")
-        self._cnf = cnf
-        self.card_dir = card_dir if card_dir is not None \
-            else Path("/home", self.username)
-        self._key = key if key else self.card_dir.joinpath(f"key-{username}.pem")
-        self._cert = cert \
-            if cert else self.card_dir.joinpath(f"cert-{username}.pem")
-        self.local = local
-
-    @property
-    def card(self):
-        return self._card
-
-    @card.setter
-    def card(self, card: card_model.Card):
-        if self._card:
-            logger.error("Delete the existing card before adding a new one.")
-            raise ValueError("A card is already assigned to this user")
-        self._card = card
-
-    @card.deleter
-    def card(self):
-        logger.info(f"Deleting the existing card from {self.username}")
-        self._card = None
-
-    @property
-    def key(self):
-        return self._key
-
-    @key.setter
-    def key(self, key: Path):
-        logger.warning("Make sure to remove the existing key/cert "
-                       "pairs before adding a new one.")
-        self._key = key
-        if self.card:
-            self.card._private_key = key
-
-    @key.deleter
-    def key(self):
-        logger.info("Deleting the current user key.")
-        self._key = None
-
-    @property
-    def cert(self):
-        return self._cert
-
-    @cert.setter
-    def cert(self, cert: Path):
-        logger.warning("Make sure to remove the existing key/cert "
-                       "pairs before adding a new one.")
-        self._cert = cert
-        if self.card:
-            self.card._cert = cert
-
-    @cert.deleter
-    def cert(self):
-        logger.info("Deleting the current user cert.")
-        self._cert = None
-
-    @property
-    def cnf(self):
-        return self._cnf
-
-    @cnf.setter
-    def cnf(self, cnf: Path):
-        if self._cnf:
-            logger.warning("Overriding current CNF file.")
-        self._cnf = cnf
-
-    @cnf.deleter
-    def cnf(self):
-        logger.info("Removing current CNF file.")
-        self._cnf = None
+            logger.info(f"User {self.username} was added to the system")
 
     def delete_user(self):
         """
-        Deletes the user and the content of user's card directory
-
-        Note: card directory would be recursively deleted with a directory
-        by itself.
+        Deletes the user
         """
         try:
             pwd.getpwnam(self.username)
             logger.info(f"Deleting the user {self.username}")
             run(['userdel', '-f', self.username], check=True)
-            if self.card_dir.exists():
-                rmtree(self.card_dir)
-                logger.debug("User's card directory "
-                             f"{str(self.card_dir)} is removed")
         except KeyError:
             pass
         logger.info(f"User {self.username} is not present on the system")
 
-    def gen_csr(self):
-        """
-        Method for generating local user specific CSR file that would be sent to
-        the local CA for generating the certificate. CSR is generated using
-        `openssl` command based on template CNF file.
-        """
-        csr_path = self.card_dir.joinpath(f"csr-{self.username}.csr")
-        cmd = ["openssl", "req", "-new", "-nodes", "-key", self._key,
-               "-reqexts", "req_exts", "-config", self._cnf, "-out", csr_path]
-        run(cmd)
-        return csr_path
-
 
 class IPAUser(User):
     """
-    This class is used to represent an IPA user.
+    This class represents an IPA user.
     """
     default_password = "redhat"
 
@@ -268,10 +122,6 @@ class IPAUser(User):
         :type username: str
         :param password: Password for the system user
         :type password: str
-        :param pin: Smart card pin for the system user
-        :type pin: str
-        :param cnf: CNF file to be associated with the user
-        :type cnf: Path
         :param key: Key to be associated with the user
         :type key: Path
         :param cert: Certificate to be associated with the user.
@@ -279,6 +129,7 @@ class IPAUser(User):
         """
 
         super().__init__(*args, **kwargs)
+        self.user_type = "ipa"
         self._meta_client = ipa_server.meta_client
         self._ipa_hostname = ipa_server.ipa_server_hostname
 
@@ -314,10 +165,7 @@ class IPAUser(User):
 
     def delete_user(self):
         """
-        Deletes the user and user's card directory.
-
-        Note: card directory would be recursively deleted with a directory
-        by itself.
+        Deletes the user
         """
         try:
             r = self._meta_client.user_del(self.username)["result"]
@@ -325,22 +173,3 @@ class IPAUser(User):
             logger.debug(r)
         except python_freeipa.exceptions.NotFound:
             pass
-        if self.card_dir.exists():
-            rmtree(self.card_dir)
-            logger.info(f"User  {self.username} directory is removed.")
-
-    def gen_csr(self):
-        """
-        Method for generating IPA user specific CSR file that would be sent to
-        the IPA server for generating the certificate. CSR is generated using
-        `openssl` command.
-        """
-        if not self._key:
-            raise SCAutolibException("Can't generate CSR because private key "
-                                     "is not set")
-        csr_path = self.card_dir.joinpath(f"csr-{self.username}.csr")
-        cmd = ["openssl", "req", "-new", "-days", "365",
-               "-nodes", "-key", self._key, "-out",
-               str(csr_path), "-subj", f"/CN={self.username}"]
-        run(cmd)
-        return csr_path
