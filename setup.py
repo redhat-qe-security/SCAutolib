38a628f667c2fc2886444d2a8538181071ed777e
Marek Havrila <mhavrila@gmail.com>
2023-12-20 09:46:09 +0100

    Bump version v3.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ graphical_reqs = [
 
 setup(
     name="SCAutolib",
-    version="2.0.0",
+    version="3.0.0",
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
