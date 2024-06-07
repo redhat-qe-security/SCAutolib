fb7c1de0c2e69de9d8f1b25164d0af97be2ca411
George Pantelakis <gpantela@redhat.com>
2024-06-07 10:51:53 +0200

    Bumping the version yet again

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ graphical_reqs = [
 
 setup(
     name="SCAutolib",
-    version="3.3.1",
+    version="3.3.2",
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
