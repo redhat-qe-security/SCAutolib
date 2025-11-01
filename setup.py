7d6f5020da77629acc404738c141780e3ac567bf
Scott Poore <spoore@redhat.com>
2025-11-01 13:11:34 -0500

    Bump minor version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ graphical_reqs = [
 
 setup(
     name="SCAutolib",
-    version="3.4.8",
+    version="3.4.9",
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
